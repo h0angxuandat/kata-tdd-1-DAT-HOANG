dd78f292cbd90af87368616c0dd91a9c6af72989
dat.hoang <0912100@gmail.com>
2015-09-26 14:26:12 +0700

    Update app to pass the unit test

--- tddkata/src/main/java/com/dathoang/tddkata/stringcaculator/StringCaculator.java
+++ tddkata/src/main/java/com/dathoang/tddkata/stringcaculator/StringCaculator.java
@@ -4,4 +4,24 @@ package com.dathoang.tddkata.stringcaculator;
  * Created by Dat Hoang on 9/26/2015.
  */
 public class StringCaculator {
+
+    public static int Add(String numbers) {
+        if(null == numbers || "" == numbers){
+            return 0;
+        }
+
+        String delimiter = ",";
+        return GetSumFromArray(numbers.split(delimiter));
+    }
+
+    private static int GetSumFromArray(String[] numbers) {
+        int sum = 0;
+
+        for(String number : numbers) {
+            sum += new Integer(number);
+        }
+
+        return sum;
+    }
+
 }
