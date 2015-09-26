592c0d8ff3175c5cc4e3caab9d2d79b56ea4476d
dat.hoang <0912100@gmail.com>
2015-09-26 14:24:50 +0700

    Update unit test for requirement: The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”

--- tddkata/src/test/java/com/dathoang/tddkata/stringcaculator/StringCaculatorTest.java
+++ tddkata/src/test/java/com/dathoang/tddkata/stringcaculator/StringCaculatorTest.java
@@ -1,8 +1,29 @@
-package com.dathoang.tddkata.stringcaculator
+package com.dathoang.tddkata.stringcaculator;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by Dat Hoang on 9/26/2015.
  */
 
 public class StringCaculatorTest {
+    @Test
+    public void TestAddWithNoAnyNumber(){
+        Assert.assertEquals(0, StringCaculator.Add(""));
+    }
+
+    @Test
+    public void TestAddWithOneNumber(){
+        Assert.assertEquals(1, StringCaculator.Add("1"));
+        Assert.assertEquals(7, StringCaculator.Add("7"));
+    }
+
+    @Test
+    public void TestAddWithTwoNumbers(){
+        Assert.assertEquals(4, StringCaculator.Add("3,1"));
+        Assert.assertEquals(16, StringCaculator.Add("5,11"));
+    }
 }
