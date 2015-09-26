9c332bea826ed1de62a1e80de4b818cd0fa0fefe
dat.hoang <0912100@gmail.com>
2015-09-26 14:49:11 +0700

    Update test for the next requirement *this requirement, my current code can pass it - it means I did a more complex work than what I need to pass the previous requirement*

--- tddkata/src/test/java/com/dathoang/tddkata/stringcaculator/StringCaculatorTest.java
+++ tddkata/src/test/java/com/dathoang/tddkata/stringcaculator/StringCaculatorTest.java
@@ -26,4 +26,11 @@ public class StringCaculatorTest {
         Assert.assertEquals(4, StringCaculator.Add("3,1"));
         Assert.assertEquals(16, StringCaculator.Add("5,11"));
     }
+
+    @Test
+    public void TestAddWithUnknowamountOfNumbers(){
+        Assert.assertEquals(78, StringCaculator.Add("5,11,3,4,55"));
+        Assert.assertEquals(45, StringCaculator.Add("1,2,3,4,5,6,7,8,9,0"));
+        Assert.assertEquals(123, StringCaculator.Add("1,2,3,4,5,6,7,8,9,0,5,11,3,4,55"));
+    }
 }
