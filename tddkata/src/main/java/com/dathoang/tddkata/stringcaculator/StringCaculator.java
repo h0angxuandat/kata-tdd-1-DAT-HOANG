56671c991771fa20d66e3a0cc142fddae7cf1c76
dat.hoang <0912100@gmail.com>
2015-09-26 15:25:27 +0700

    Update app to pass the new test

--- tddkata/src/main/java/com/dathoang/tddkata/stringcaculator/StringCaculator.java
+++ tddkata/src/main/java/com/dathoang/tddkata/stringcaculator/StringCaculator.java
@@ -4,14 +4,15 @@ package com.dathoang.tddkata.stringcaculator;
  * Created by Dat Hoang on 9/26/2015.
  */
 public class StringCaculator {
+    
+    private static final String REGEX = "[,\n]";
 
     public static int Add(String numbers) {
         if(null == numbers || "" == numbers){
             return 0;
         }
 
-        String delimiter = ",";
-        return GetSumFromArray(numbers.split(delimiter));
+        return GetSumFromArray(numbers.split(REGEX));
     }
 
     private static int GetSumFromArray(String[] numbers) {
